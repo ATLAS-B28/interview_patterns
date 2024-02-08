ca063ee5b5509842d4886e3a1ac1dd966b921a8a
ATLAS-B28 <adityacbhambere28@gmail.com>
2024-02-08 17:27:54 +0530

    changes made

--- src/main/java/TwoPointers/Q6.java
+++ src/main/java/TwoPointers/Q6.java
@@ -1,34 +1,32 @@
 package TwoPointers;
 //Problem Challenge 2 - Comparing Strings containing Backspaces
 
-import java.util.Stack;
+
 public class Q6 {
 
     public static boolean compareStrings(String str1, String str2) {
-       Stack<Character> stack1 = new Stack<>();
-       Stack<Character> stack2 = new Stack<>();
+        int i = 0;
+        int j = 0;
+        
+        while(i < str1.length() && j < str2.length()) {
+            if(str1.charAt(i) != str2.charAt(j)) {
+                System.out.println("Strings are not equal");
+                return false;
+            }
 
-       for(char c : str1.toCharArray()) {
-           if(c != '#') {
-               stack1.push(c);
-           } else if (!stack1.isEmpty()) {
-               stack1.pop();
-           }
-       }
+            i++;
+            j++;
+        }
 
-       for(char c : str2.toCharArray()) {
-           if(c != '#') {
-               stack2.push(c);
-           } else if (!stack2.isEmpty()) {
-               stack2.pop();
-           }
-       }
-
-       return stack1.equals(stack2);
+        if(str1.length() == str2.length()) {
+           return true;
+        } else {
+            return false;
+        }
     }
 
     public static void main(String[] args) {
-        String str1 = "xy#z";
+        String str1 = "xzz#";
         String str2 = "xzz#";
         System.out.println("Are the strings equal? "+compareStrings(str1, str2));
     }
